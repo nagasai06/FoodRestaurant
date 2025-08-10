03c43d7b78541ae8e3a732f82cfd49eb15d680a3
venkata <nagasaigudlavalleti06@gmail.com>
2025-08-09 20:32:09 -0400

    Corrected PubSubAppender 1.0

--- src/main/java/com/RestaurantFood/FoodRestaurant/PubSubAppender.java
+++ src/main/java/com/RestaurantFood/FoodRestaurant/PubSubAppender.java
@@ -1,18 +1,18 @@
 package com.RestaurantFood.FoodRestaurant;
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class PubSubAppender extends AppenderBase<ILoggingEvent> {
 
-    private Publisher publisher;
+    private static volatile Publisher publisher; // one shared instance
     private String projectId;
     private String topicId;
 
@@ -25,41 +25,63 @@ public class PubSubAppender extends AppenderBase<ILoggingEvent> {
     }
 
     @Override
-    public void start(){
-
-        try {
-            ProjectTopicName topicName = ProjectTopicName.of(projectId,topicId);
-            publisher = publisher.newBuilder(topicName).build();
-            super.start();
-        } catch (Exception e){
-            addError("Failed to start Pub/Sub",e);
+    public void start() {
+        if (this.projectId == null || this.topicId == null) {
+            addError("Pub/Sub projectId or topicId is not set.");
+            return;
         }
-    }
 
-    @Override
-    public void stop(){
         try {
-            if(publisher!=null){
-                publisher.shutdown();
-                publisher.awaitTermination(1, TimeUnit.MINUTES);
-
+            if (publisher == null) {
+                synchronized (PubSubAppender.class) {
+                    if (publisher == null) {
+                        ProjectTopicName topicName = ProjectTopicName.of(projectId, topicId);
+                        publisher = Publisher.newBuilder(topicName).build();
+                        addInfo("Pub/Sub publisher initialized for topic: " + topicId);
+                    }
+                }
             }
-        } catch (Exception e){
-            addError("Unable to shutdown",e);
+            super.start();
+        } catch (IOException e) {
+            addError("Failed to initialize Pub/Sub publisher", e);
         }
-        super.stop();
     }
 
     @Override
     protected void append(ILoggingEvent eventObject) {
+        if (publisher == null) {
+            return;
+        }
         try {
             String logMsg = eventObject.getFormattedMessage();
-            ByteString byteString = ByteString.copyFromUtf8(logMsg);
-            PubsubMessage message= PubsubMessage.newBuilder().setData(byteString).build();
+            ByteString data = ByteString.copyFromUtf8(logMsg);
 
-            publisher.publish(message);
-        } catch(Exception e){
-            addError("Unable to publish",e);
+            PubsubMessage message = PubsubMessage.newBuilder()
+                    .setData(data)
+                    .putAttributes("level", eventObject.getLevel().toString())
+                    .putAttributes("logger", eventObject.getLoggerName())
+                    .putAttributes("timestamp", String.valueOf(eventObject.getTimeStamp()))
+                    .build();
+
+            publisher.publish(message); // async publish
+
+        } catch (Exception e) {
+            addError("Failed to publish log message to Pub/Sub", e);
         }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            if (publisher != null) {
+                publisher.shutdown();
+                publisher.awaitTermination(1, TimeUnit.MINUTES);
+                publisher = null;
+                addInfo("Pub/Sub publisher shut down.");
+            }
+        } catch (Exception e) {
+            addError("Error shutting down Pub/Sub publisher", e);
+        }
+        super.stop();
     }
 }
